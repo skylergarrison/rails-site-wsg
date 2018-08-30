ddf2caa202d76122dc14994e8bfb2d0532c5dd4c
Skyler Garrison <42485938+skylergarrison@users.noreply.github.com>
2018-08-29 22:24:34 -0700

    Added resume pdf and link, changed page titles

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,16 @@ module MySite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    
+    def download
+      file = "#{Rails.root}/skyler-resume.pdf"
+      if File.exists?(file)
+        send_file file, :type=>"application/pdf", :x_sendfile=>true
+      else
+        flash[:notice] = 'File not found'
+        redirect_to :index
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
